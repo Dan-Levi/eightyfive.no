6cb3c79d48608514c80bf5f6cb38f173a8417241
Dan-Levi Tømta <danbannan@gmail.com>
2026-03-24 16:57:04 +0100

    added ecosystem edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
             instances: 1,
             exec_mode: "fork",
             watch: false,
+            autorestart: true,
             env: {
                 NODE_ENV: "production"
             }
